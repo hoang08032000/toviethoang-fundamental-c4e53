0576135405f41111ddbfc24460954de0085faedc
hoang08032000 <56767074+hoang08032000@users.noreply.github.com>
2019-10-28 18:39:05 +0700

    Update Shop.js

--- session3/Homework-3/Shop.js
+++ session3/Homework-3/Shop.js
@@ -3,11 +3,15 @@ let loop = true
 while(loop){
     let key = prompt("C/R/U/D?").toLowerCase()
     if(key == "r"){
-        console.log(shop)
+        shop.forEach((v,i) =>{
+            console.log(`${i+1} : ${v}`)
+        })
     } else if(key == "c"){
         let created = prompt("Mặt hàng bạn muốn thêm")
         shop.push(created)
-        console.log(shop)
+        shop.forEach((v,i) =>{
+            console.log(`${i+1} : ${v}`)
+        })
     } else if(key == "u"){
         let Updated = prompt("Bạn muốn thêm theo Value hay index? v/i?").toLowerCase()
         if(Updated == "i"){
@@ -16,14 +20,18 @@ while(loop){
                 alert("Nhập sai rồi bạn êi")
             } else{
                 shop[indexUpdated-1] = prompt("Bạn muốn đổi thành gì nào?")
-                console.log(shop)
+                shop.forEach((v,i) =>{
+                    console.log(`${i+1} : ${v}`)
+                    })
             }
         } else if(Updated == "v"){
             let valueUpdate = prompt("Nhập mặt hàng bạn muốn thay đổi")
             if(shop.includes(valueUpdate)){
                 let indexUpdated = shop.indexOf(valueUpdate)
                 shop[indexUpdated] = prompt("Bạn muốn đổi thành gì nào?")
-                console.log(shop)
+                shop.forEach((v,i) =>{
+                    console.log(`${i+1} : ${v}`)
+                    })
             }
         } else{
             alert("Lỗi rồi bạn êi")
@@ -36,14 +44,18 @@ while(loop){
                 alert("Nhập sai rồi bạn êi")
             } else{
                 shop[indexDeleted-1] = prompt("Bạn muốn đổi thành gì nào?")
-                console.log(shop)
+                shop.forEach((v,i) =>{
+                    console.log(`${i+1} : ${v}`)
+                })
             }
         } else if(Deleted == "v"){
             let valueDeleted = prompt("Nhập mặt hàng bạn muốn thay đổi")
             if(shop.includes(valueDeleted)){
                 let indexDeleted = shop.indexOf(valueDeleted)
                 shop[indexDeleted] = prompt("Bạn muốn đổi thành gì nào?")
-                console.log(shop)
+                shop.forEach((v,i) =>{
+                    console.log(`${i+1} : ${v}`)
+                })
             }
         } else{
             alert("Lỗi rồi bạn êi")
