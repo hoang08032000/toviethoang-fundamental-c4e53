8b5f74072dff4517b5e6a18c2194592afaed7c38
hoang08032000 <56767074+hoang08032000@users.noreply.github.com>
2019-11-07 23:05:22 +0700

    Update inside.js

--- HomeWork/inside.js
+++ HomeWork/inside.js
@@ -1,12 +1,14 @@
 function is_inside(a=[],b=[]){
     if(a[0]<b[0] || a[1]<b[1]){
         return false
-    } else{
+    } else if(a[0] > b[0]+b[2] || a[1] > b[1]+b[3]){
+        return false
+    }else if(a[0]>b[0]){
         return true
     }
 }
-let a = [100, 120]
-let b = [140, 60]
+let a = [300, 500]
+let b = [140, 60,100,400]
 let result
 if(is_inside(a,b)==true){
     result = "inside"
@@ -18,3 +20,4 @@ if(result === "outside" || result === "inside"){
 } else{
     console.log("Ooops, bugs detected")
 }
+console.log(result)
